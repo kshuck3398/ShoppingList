44eaf08dd6fbc5a489b62d88336735615a74cb45
Kellen Shuck <kshuck3398@stu.d214.org>
2020-02-03 11:36:54 -0600

    Connect Tableview, Button, and Textfield to assistant

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var newItemTextfield: UINavigationItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func whenButtonPressed(_ sender: Any) {
+        
+        
+        
+    }
+    
 
 }
 
