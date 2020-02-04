4ac29fca220807c1673848ef39e08ff656921943
Kellen Shuck <kshuck3398@stu.d214.org>
2020-02-04 11:16:11 -0600

    New File: Item

---
+++ ShoppingList/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ShoppingList
+//
+//  Created by Kellen Shuck on 2/4/20.
+//  Copyright © 2020 KellenShuck. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+}
--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -8,21 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource{
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var newItemTextfield: UINavigationItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+
     }
     @IBAction func whenButtonPressed(_ sender: Any) {
         
         
-        
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+    
 }
 
